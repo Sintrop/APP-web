8e72a4e2d1ff27299eea376e5b343cfc52b13e77
Bruno Mestres <brunomestres@gmail.com>
2023-01-22 20:42:57 -0300

    Add swr

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-import React, { useState, useEffect } from "react";
-import Web3 from "web3";
-import Router from "./routes";
-import Activist from "./data/contracts/abis/ActivistContract.json";
-import "./app.css";
-function App() {
-  const [networkWallet, setNetworkWallet] = useState();
-  const NetworkContract = Object.keys(Activist.networks).toString();
-  const web3 = new Web3(window.ethereum);
-  web3.eth.net.getId().then((data) => setNetworkWallet(String(data)));
+import React from "react";
 
-  if (networkWallet === NetworkContract) {
+import Router from "./routes";
+import "./app.css";
+import { useNetwork } from "./hooks/useNetwork";
+function App() {
+  const {  data, isSupported } = useNetwork()
+  console.log(data)
+  if (isSupported) {
     return <Router />;
   } else {
     return< div className="background">
