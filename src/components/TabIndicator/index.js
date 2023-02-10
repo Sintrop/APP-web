6bc0e3316f2218b2e146608d7d6e2c65ba7ef397
Emanoel Augusto Freires <emanoelaugusto7@gmail.com>
2023-02-09 22:05:18 -0300

    improve system register 2

--- src/components/TabIndicator/index.js
+++ src/components/TabIndicator/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './tabs.css';
+import {useNavigate} from 'react-router';
 
 export default function TabIndicator({activeTab, wallet}){
+    const navigate = useNavigate();
     return(
         <div className='container-tabs'>
-            <a href={`/dashboard/${wallet}`}>dashboard </a>
+            <a
+                onClick={() => navigate(`/dashboard/${wallet}/isa/main`)}
+                style={{textDecoration: 'underline', color: 'blue', cursor: 'pointer'}}
+            >dashboard </a>
             <p> | {activeTab}</p>
         </div>
     )
