e7de25e8a65b3edb24db7cf38fd741f7b1f248bc
Emanoel Augusto <emanoelaugusto7@gmail.com>
2024-03-19 15:34:39 -0300

    feat: displayed messages eith images and send message

--- src/components/Chat/components/MessageItem.js
+++ src/components/Chat/components/MessageItem.js
@@ -1,10 +1,12 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { api } from "../../../services/api";
 import { useMainContext } from "../../../hooks/useMainContext";
 import { getImage } from "../../../services/getImage";
+import CryptoJS from 'crypto-js';
+import format from "date-fns/format";
 
-export function MessageItem({data}){
-    const {userData} = useMainContext();
+export function MessageItem({ data }) {
+    const { userData } = useMainContext();
     const [isMessageUser, setIsMessageUser] = useState(false);
     const [textMessage, setTextMessage] = useState('');
     const [image, setImage] = useState(null);
@@ -20,6 +22,7 @@ export function MessageItem({data}){
 
     useEffect(() => {
         visualizedMsg();
+        decryptMessage(data?.message);
 
         if (data.type === 'image') {
             const photos = JSON.parse(data.photos);
@@ -44,10 +47,31 @@ export function MessageItem({data}){
             messageId: data.id
         })
     }
-    return(
-        <div className={`flex flex-col min-h-[50px] ${isMessageUser ? 'items-end mb-2' : 'items-start mb-4'}`}>
+
+    function decryptMessage(msg) {
+        const decrypt = CryptoJS.AES.decrypt(msg, process.env.REACT_APP_DECRYPT_MESSAGE_KEY);
+        setTextMessage(decrypt.toString(CryptoJS.enc.Utf8));
+    }
+
+    return (
+        <div className={`flex flex-col mb-2 ${isMessageUser ? 'items-end' : 'items-start'}`}>
             <div className={`${isMessageUser ? 'bg-[#00FF84]' : 'bg-[#02c0a1]'} p-2 rounded-md min-w-[120px] max-w-[200px]`}>
-                <p>{data?.message}</p>
+                {data.type === 'image' && (
+                    <div className="w-[150px] h-[150px] rounded-md bg-gray-400">
+                        {image && (
+                            <img
+                                src={image}
+                                className="w-[150px] h-[150px] rounded-md object-cover"
+                            />
+                        )}
+                    </div>
+                )}
+                <p>{textMessage}</p>
+                {data.createdAt && (
+                    <p className={`${isMessageUser ? 'text-right' : 'text-left'} text-xs text-gray-800 mt-1`}>
+                        {format(new Date(), 'dd/MM/yyyy') === format(new Date(data.createdAt), 'dd/MM/yyyy') ? format(new Date(data.createdAt), 'kk:mm') : format(new Date(data.createdAt), 'dd/MM/yyyy')}
+                    </p>
+                )}
             </div>
         </div>
     )
--- src/components/Chat/components/ModalMessages.js
+++ src/components/Chat/components/ModalMessages.js
@@ -2,11 +2,15 @@ import React, {useState, useEffect} from "react";
 import * as Dialog from '@radix-ui/react-dialog';
 import { ToastContainer, toast } from "react-toastify";
 import {IoMdCloseCircleOutline} from 'react-icons/io';
+import { MdSend } from "react-icons/md";
 import { api } from "../../../services/api";
 import { ActivityIndicator } from "../../ActivityIndicator";
 import { MessageItem } from "./MessageItem";
+import CryptoJS from "crypto-js";
+import { useMainContext } from "../../../hooks/useMainContext";
 
 export function ModalMessages({chat, imageProfile, participant, socket}) {
+    const {userData} = useMainContext();
     const [sending, setSending] = useState(false);
     const [inputMessage, setInputMessage] = useState('');
     const [messagesList, setMessagesList] = useState([]);
@@ -16,8 +20,27 @@ export function ModalMessages({chat, imageProfile, participant, socket}) {
     const [imageToSend, setImageToSend] = useState(null);
 
     useEffect(() => {
+        if (!socket) return;
+        socket.on('new_message', data => {
+            reicevedMessage(data);
+        })
+
+    }, [socket]);
+
+    useEffect(() => {
         getMessages();
     }, []);
+
+    function reicevedMessage(message_data) {
+        if (chat.chatId !== message_data.chatId) {
+            return;
+        }
+        let array = [];
+        array = messagesList;
+        array.unshift(message_data);
+        setMessagesList(array);
+        getMessages();
+    }
 
     async function getMessages() {
         setLoading(true);
@@ -27,6 +50,41 @@ export function ModalMessages({chat, imageProfile, participant, socket}) {
 
         setFirstLoad(false);
         setLoading(false);
+    }
+
+    async function sendMessage() {
+        let hashPhotos = [];
+
+        if(!imageToSend && !inputMessage.trim()){
+            return;
+        }
+
+        setSending(true);
+        // if(imageToSend){
+        //     const hash = await addPhotoIPFS(imageToSend);
+        //     hashPhotos.push(hash);
+        // }
+
+        const encrypt = CryptoJS.AES.encrypt(inputMessage, process.env.REACT_APP_DECRYPT_MESSAGE_KEY);
+
+        try {
+            await api.post('/chat/message', {
+                chatId: chat.chatId,
+                message: encrypt.toString(),
+                type: imageToSend ? 'image' : 'text',
+                userId: userData.id,
+                userData: JSON.stringify(userData),
+                participantData: JSON.stringify(participant),
+                photos: JSON.stringify(hashPhotos)
+            })
+        } catch (err) {
+            console.log(err)
+        }
+        await socket.emit('message', { message: encrypt.toString(), chatId: chat.chatId, userId: userData.id });
+        setInputMessage('');
+        getMessages();
+        setImageToSend(null);
+        setSending(false);
     }
 
     return (
@@ -58,12 +116,28 @@ export function ModalMessages({chat, imageProfile, participant, socket}) {
                         />
                     ))}
 
-                    {loading && (
+                    {loading && firstLoad && (
                         <ActivityIndicator size={50}/>
                     )}
                 </div>
 
-                <div />
+                <div className="flex items-center w-full p-1">
+                    <input
+                        value={inputMessage}
+                        onChange={(e) => setInputMessage(e.target.value)}
+                        placeholder="Digite aqui"
+                        className="w-[90%] h-10 bg-white rounded-full px-3 text-black"
+                    />
+
+                    <div className="flex items-center justify-center w-[15%]">
+                        <button
+                            className="h-10 w-10 rounded-full flex items-center justify-center bg-green-600"
+                            onClick={sendMessage}
+                        >
+                            <MdSend color='white' size={20} />
+                        </button>
+                    </div>
+                </div>
 
             </Dialog.Content>
 
