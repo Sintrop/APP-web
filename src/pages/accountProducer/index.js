8afd365a67c76939bc05d3be5bc84775362f39ba
Emanoel Augusto <emanoelaugusto7@gmail.com>
2024-04-05 10:36:25 -0300

    feat: new vesion account producer

--- src/pages/accountProducer/index.js
+++ src/pages/accountProducer/index.js
@@ -1,7 +1,7 @@
-import React, {useState, useEffect} from 'react';
-import {useParams} from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import './accountProducer.css';
-import {get} from '../../config/infura';
+import { get } from '../../config/infura';
 import * as Dialog from '@radix-ui/react-dialog';
 import { IsProducerSyntropic } from '../../components/IsProducerSyntropic';
 import { useTranslation } from 'react-i18next';
@@ -10,15 +10,18 @@ import { api } from '../../services/api';
 import { GoogleMap, LoadScript, DrawingManager, Marker, Polyline } from '@react-google-maps/api';
 
 //services
-import {GetDelation, GetInspections, GetProducer} from '../../services/accountProducerService';
+import { GetDelation, GetInspections, GetProducer } from '../../services/accountProducerService';
 
 //components
 import ItemInspection from '../../components/ProducerPageComponents/ItemInspection';
 import { InspectionItemResult } from './inspectionItemResult';
-import {Warning} from '../../components/Warning';
-import {ModalChooseLang} from '../../components/ModalChooseLang';
+import { Warning } from '../../components/Warning';
+import { ModalChooseLang } from '../../components/ModalChooseLang';
 import { useMainContext } from '../../hooks/useMainContext';
 import { ModalViewPhoto } from '../../components/ModalViewPhoto';
+import { getImage } from '../../services/getImage';
+import { ActivityIndicator } from '../../components/ActivityIndicator';
+import { ProducerGraphics } from '../../components/ProducerGraphics';
 
 const containerStyle = {
     width: '100%',
@@ -28,10 +31,15 @@ const containerStyle = {
     borderColor: '#3E9EF5'
 };
 
-export default function AccountProducer(){
-    const {t} = useTranslation();
-    const {walletSelected} = useParams();
-    const {modalChooseLang} = useMainContext();
+const containerMapStyle = {
+    width: '100%',
+    height: '300px',
+};
+
+export default function AccountProducer() {
+    const { t } = useTranslation();
+    const { walletSelected } = useParams();
+    const { modalChooseLang } = useMainContext();
     const [producerData, setProducerData] = useState([]);
     const [producerAddress, setProducerAddress] = useState([]);
     const [center, setCenter] = useState({})
@@ -41,13 +49,17 @@ export default function AccountProducer(){
     const [modalChooseTypeDelation, setModalChooseTypeDelation] = useState(false);
     const [modalViewPhoto, setModalViewPhoto] = useState(false);
     const [hashSelected, setHashSelected] = useState('');
+    const [imageProfile, setImageProfile] = useState(null);
+    const [userData, setUserData] = useState({});
+    const [initialRegion, setInitialRegion] = useState(null);
+    const [loadingInspections, setLoadingInspections] = useState(false);
 
     useEffect(() => {
         getProducer();
         getProducerApi();
-    },[]);
+    }, []);
 
-    async function getProducer(){
+    async function getProducer() {
         const response = await GetProducer(walletSelected);
         setProducerData(response);
         fixCoordinates(JSON.parse(response.propertyAddress.coordinate));
@@ -57,166 +69,166 @@ export default function AccountProducer(){
         getInspections();
     }
 
-    async function fixCoordinates(coords){
+    async function fixCoordinates(coords) {
         const arrayLat = String(coords.lat).split('');
-            const arrayLng = String(coords.lng).split('');
-            let newLat = '';
-            let newLng = '';
+        const arrayLng = String(coords.lng).split('');
+        let newLat = '';
+        let newLng = '';
 
-            for(var i = 0; i < arrayLat.length; i++){
-                if(i === 3){
-                    if(arrayLat[i] === '.'){
-                        newLat += arrayLat[i]
-                    }else{
-                        if(arrayLat[i] === ','){
-                            newLat += '.'
-                        }else{
-                            newLat += `.${arrayLat[i]}`
-                        }
-                    }
-                }else{
+        for (var i = 0; i < arrayLat.length; i++) {
+            if (i === 3) {
+                if (arrayLat[i] === '.') {
                     newLat += arrayLat[i]
-                }
-
-            }
-
-            for(var i = 0; i < arrayLng.length; i++){
-                if(i === 3){
-                    if(arrayLng[i] === '.'){
-                        newLng += arrayLng[i]
-                    }else{
-                        if(arrayLng[i] === ','){
-                            newLng += '.'
-                        }else{
-                            newLng += `.${arrayLng[i]}`
-                        }
+                } else {
+                    if (arrayLat[i] === ',') {
+                        newLat += '.'
+                    } else {
+                        newLat += `.${arrayLat[i]}`
                     }
-                }else{
-                    newLng += arrayLng[i]
                 }
+            } else {
+                newLat += arrayLat[i]
             }
-            setCenter({
-                lat: Number(newLat),
-                lng: Number(newLng)
-            })
+
+        }
+
+        for (var i = 0; i < arrayLng.length; i++) {
+            if (i === 3) {
+                if (arrayLng[i] === '.') {
+                    newLng += arrayLng[i]
+                } else {
+                    if (arrayLng[i] === ',') {
+                        newLng += '.'
+                    } else {
+                        newLng += `.${arrayLng[i]}`
+                    }
+                }
+            } else {
+                newLng += arrayLng[i]
+            }
+        }
+        setCenter({
+            lat: Number(newLat),
+            lng: Number(newLng)
+        })
     }
 
-    async function getInspections(){
-        const response = await GetInspections();
-        const filterInspections = response.filter(item => String(item.createdBy).toUpperCase() === walletSelected.toUpperCase())
-        setInspections(filterInspections);
+    async function getInspections() {
+        setLoadingInspections(true);
+
+        let array = [];
+        const response = await api.get('/web3/history-inspections');
+        array = response.data.inspections
+
+        const filter = array.filter(item => String(item.createdBy).toUpperCase() === String(walletSelected).toUpperCase());
+        setInspections(filter);
+        
+        setLoadingInspections(false);
     }
 
-    async function getBase64(path){
+    async function getBase64(path) {
         const base64 = await get(path);
         setProofPhotoBase64(base64);
     }
 
-    async function getProducerApi(){
-        try{
+    async function getProducerApi() {
+        try {
             const response = await api.get(`/user/${String(walletSelected).toUpperCase()}`);
             const address = JSON.parse(response.data.user?.address)
             setProducerAddress(address);
-            console.log(response.data.user)
-        }catch(err){
+            setUserData(response.data.user);
+            getImageProfile(response.data.user.imgProfileUrl);
+            if (response.data.user.geoLocation) {
+                setInitialRegion(JSON.parse(response.data.user.geoLocation));
+            }
+        } catch (err) {
             console.log(err)
         }
     }
 
-    return(
-        <div className="w-full flex flex-col items-center bg-green-950 h-[100vh]">
-            
-            <div className='w-full h-20 bg-gradient-to-r from-[#FFD875] to-[#461D03] flex items-center justify-center lg:justify-start lg:px-16 mb-3'>
-                <img
-                    src={require('../../assets/logo-branco.png')}
-                    className='w-[170px] object-contain'
-                />
-            </div>
+    async function getImageProfile(hash) {
+        const response = await getImage(hash);
+        setImageProfile(response);
+    }
 
-            <div className='flex flex-col w-full items-center overflow-auto'>
-            <Warning
-                message='Data from our test network'
-                width={250}
+    return (
+        <div className="w-full flex flex-col items-center bg-green-950 h-[100vh] pt-5 overflow-y-auto overflow-x-hidden px-2">
+            <img
+                src={require('../../assets/logo-branco.png')}
+                className='w-[140px] lg:w-[170px] object-contain'
             />
 
-            <div className='flex flex-col lg:w-[1000px] lg:flex-row w-full gap-5 lg:gap-10 justify-center items-center lg:px-30 lg:mt-10'>
-                <img 
-                    src={`data:image/png;base64,${proofPhotoBase64}`} 
-                    className='h-[200px] w-[200px] object-cover border-4 border-[#3E9EF5] rounded-full mt-5 lg:mt-0'
+            <div className='flex flex-col items-center lg:w-[1000px] lg:flex-row w-full gap-5 mt-5 lg:gap-5 lg:px-30 lg:mt-10 bg-[#0a4303] rounded-md p-3'>
+                <img
+                    src={imageProfile ? imageProfile : require('../../assets/token.png')}
+                    className='h-[150px] w-[150px] lg:h-[200px] lg:w-[200px] object-cover border-4  rounded-full mt-5 lg:mt-0'
                 />
 
-                <div className='flex flex-col'>
-                    <h1 className='font-bold text-center lg:text-left text-2xl text-white'>{producerData?.name}</h1>
-                    <p className='text-lg text-center lg:text-left text-white mt-2'>{producerAddress?.city}/{producerAddress?.state}, {producerAddress?.street}</p>
-                    <p className='text-lg text-center lg:text-left text-white'>{t('Inspections Received')}: {inspections?.length}</p>
-                    <div className='flex w-full justify-center lg:justify-start'>
-                        <IsProducerSyntropic data={producerData}/>
-                    </div>
-                </div>
-
-                <div className='flex flex-col'>
-                    <p className='text-lg text-center lg:text-left text-white mt-2'>{t('Regeneration Score')}: {producerData?.isa?.isaScore}</p>
-                    <p className='text-lg text-center lg:text-left text-white'>{t('Average Regeneration')}: {(Number(producerData?.isa?.isaScore) / Number(producerData?.totalInspections)).toFixed(1).replace('.',',')}</p>
-                    <p className='text-lg text-center lg:text-left text-white'>{t('Delations Received')}: 0</p>
-                </div>
-
-                <div className='flex flex-col'>
-                    <Dialog.Root open={modalChooseTypeDelation} onOpenChange={(open) => setModalChooseTypeDelation(open)}>
-                        <ModalChooseTypeDelation/>
-                        <Dialog.Trigger
-                            className='px-3 lg:px-8 py-3 bg-[#FF9900] rounded-md font-bold text-[#062C01]'
-                        >
-                            {t('Report Producer')}
-                        </Dialog.Trigger>
-                    </Dialog.Root>
+                <div className='flex flex-col items-center lg:items-start'>
+                    <h1 className='font-bold text-center lg:text-left lg:text-2xl text-white'>{userData?.name}</h1>
+                    <h3 className='text-center lg:text-left lg:text-lg text-white max-w-[78%] text-ellipsis overflow-hidden lg:max-w-full'>{walletSelected}</h3>
+                    <p className='lg:text-lg text-center lg:text-left text-white mt-2'>{producerAddress?.city}/{producerAddress?.state}, {producerAddress?.street}</p>
                 </div>
             </div>
 
-            {producerData && (
-                <div className='flex w-full lg:w-[1000px] justify-center mt-5 px-2 lg:px-0 lg:mt-10'>
-                    <div className='flex w-full justify-center'>
-                    <LoadScript
-                        googleMapsApiKey='AIzaSyD9854_llv58ijiMNKxdLbe6crnQuCpGuo'
-                        libraries={['drawing']}
-                    >
-                        <GoogleMap
-                            mapContainerStyle={containerStyle}
-                            center={center}
-                            zoom={18}
-                            mapTypeId='satellite'
-                        >
-                            { 
-                                <Marker position={center}/>
-                            }
-            
+            <div className='flex flex-col lg:w-[1000px] w-full gap-5 mt-5 lg:gap-5 lg:px-30 bg-[#0a4303] rounded-md p-3'>
+                <h3 className='font-bold text-white text-center lg:text-left lg:text-lg'>Estatísticas do(a) produtor(a)</h3>
 
-                        
-                                {/* <Polyline
-                                    path={pathPolyline}
-                                /> */}
-                            
-                        </GoogleMap>
-                    </LoadScript>
+                <div className='flex items-center justify-center flex-wrap gap-5 mt-5'>
+                    <div className='flex flex-col items-center gap-1 min-w-[100px] lg:min-w-[150px]'>
+                        <p className='font-bold text-white text-xl lg:text-3xl'>{producerData?.totalInspections} </p>
+                        <p className='text-white text-xs lg:text-base'>Inspeções recebidas</p>
+                    </div>
+
+                    <div className='flex flex-col items-center gap-1 min-w-[100px] lg:min-w-[150px]'>
+                        <p className='font-bold text-white text-xl lg:text-3xl'>{producerData?.isa?.isaScore}</p>
+                        <p className='text-white text-xs lg:text-base'>Pontuação</p>
+                    </div>
+
+                    <div className='flex flex-col items-center gap-1 min-w-[100px] lg:min-w-[150px]'>
+                        <p className='font-bold text-white text-xl lg:text-3xl'>{Intl.NumberFormat('pt-BR', { maximumFractionDigits: 2 }).format(producerData?.isa?.isaScore / producerData?.totalInspections)}</p>
+                        <p className='text-white text-xs lg:text-base'>Média</p>
+                    </div>
+
+                    <div className='flex flex-col items-center gap-1 min-w-[100px] lg:min-w-[150px]'>
+                        <p className='font-bold text-white text-xl lg:text-3xl'>0</p>
+                        <p className='text-red-500 text-xs lg:text-base'>Denúncias recebidas</p>
                     </div>
                 </div>
-            )}
 
-            <div className="flex flex-col w-full lg:w-[1000px] mt-5 lg:mt-10">
-            {inspections.map(item => (
-                <InspectionItemResult
-                    key={item.id}
-                    data={item}
-                    initialVisible
-                />
-            ))}
+                <ProducerGraphics inspections={inspections}/>
             </div>
 
-        
-            <Dialog.Root
-                open={modalChooseLang}
-            >
-                <ModalChooseLang/>
-            </Dialog.Root>
+            <div className='flex flex-col lg:w-[1000px] w-full gap-5 mt-5 lg:gap-5 lg:px-30 bg-[#0a4303] rounded-md p-3'>
+                <h3 className='font-bold text-white text-center lg:text-left lg:text-lg'>Mapa da propriedade</h3>
+                <div className="flex items-center justify-center bg-gray-400 rounded-md w-full h-[300px]">
+                    {initialRegion ? (
+                        <LoadScript
+                            googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_KEY}
+                            libraries={['drawing']}
+                        >
+                            <GoogleMap
+                                mapContainerStyle={containerMapStyle}
+                                center={{ lat: initialRegion?.latitude, lng: initialRegion?.longitude }}
+                                zoom={15}
+                                mapTypeId="hybrid"
+                            >
+
+                                <Marker
+                                    position={{ lat: initialRegion?.latitude, lng: initialRegion?.longitude }}
+                                />
+
+                            </GoogleMap>
+
+                        </LoadScript>
+                    ) : (
+                        <ActivityIndicator size={60} />
+                    )}
+                </div>
+            </div>
+
+            <div className='flex flex-col lg:w-[1000px] w-full gap-5 mt-5 lg:gap-5 lg:px-30 bg-[#0a4303] rounded-md p-3 mb-10'>
+                <h3 className='font-bold text-white text-center lg:text-left lg:text-lg'>Inspeções recebidas</h3>
             </div>
         </div>
     )
