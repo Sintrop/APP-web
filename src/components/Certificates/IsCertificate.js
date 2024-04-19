af19c660fef436013ba39f56a4a49de9ae9a85ed
Emanoel Augusto <emanoelaugusto7@gmail.com>
2024-04-19 14:17:48 -0300

    feat: new design certificate

--- src/components/Certificates/IsCertificate.js
+++ src/components/Certificates/IsCertificate.js
@@ -20,21 +20,21 @@ export function IsCertified({producer}){
         <div className={`flex items-center gap-1 rounded-md p-1 w-fit ${status === 2 ? 'bg-red-500' : 'bg-[#0a4303]'}`}>
             {status === 0 && (
                 <>
-                <FaClock size={20} color='white' />
+                <FaClock size={15} color='white' />
                 <p className="text-white font-bold text-xs">Certificação em andamento</p>
                 </>
             )}
 
             {status === 1 && (
                 <>
-                <FaCheck size={20} color='white' />
+                <FaCheck size={15} color='white' />
                 <p className="text-white font-bold text-xs">Produtor aprovado</p>
                 </>
             )}
 
             {status === 2 && (
                 <>
-                <FaRegWindowClose size={20} color='white' />
+                <FaRegWindowClose size={15} color='white' />
                 <p className="text-white font-bold text-xs">Produtor reprovado</p>
                 </>
             )}
