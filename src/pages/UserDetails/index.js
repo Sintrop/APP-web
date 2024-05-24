3d10b541165ef3ee1df8b6741f0b5db5791707ff
Emanoel Augusto <emanoelaugusto7@gmail.com>
2024-05-24 07:49:50 -0300

    fix: image profile

--- src/pages/UserDetails/index.js
+++ src/pages/UserDetails/index.js
@@ -184,7 +184,7 @@ export function UserDetails() {
                                                 {imageProfile ? (
                                                     <img
                                                         src={imageProfile}
-                                                        className="rounded-full object-cover"
+                                                        className="rounded-full object-cover w-full h-full"
                                                     />
                                                 ) : (
                                                     <>
