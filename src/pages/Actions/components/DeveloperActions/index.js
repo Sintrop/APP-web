fcd0a2da47516615753b60235473ca8e91b66179
Emanoel Augusto <emanoelaugusto7@gmail.com>
2024-06-11 14:20:34 -0300

    feat: add invite developer

--- src/pages/Actions/components/DeveloperActions/index.js
+++ src/pages/Actions/components/DeveloperActions/index.js
@@ -5,6 +5,10 @@ import { FeedbackItem } from "./feedbackItem";
 import { useMainContext } from "../../../../hooks/useMainContext";
 import { SendReportDev } from '../../../checkout/SendReportDev';
 import { UserRankingItem } from "../../../Ranking/components/UserRankingItem";
+import { Invite } from "../../../../services/invitationService";
+import { LoadingTransaction } from "../../../../components/LoadingTransaction";
+import * as Dialog from '@radix-ui/react-dialog';
+import { ToastContainer, toast } from "react-toastify";
 
 export function DeveloperActions() {
     const { userData, walletConnected } = useMainContext();
@@ -14,10 +18,14 @@ export function DeveloperActions() {
     const [modalDevReport, setModalDevReport] = useState(false);
     const [tabSelected, setTabSelected] = useState('users');
     const [users, setUsers] = useState([]);
+    const [wallet, setWallet] = useState('');
+    const [loadingTransaction, setLoadingTransaction] = useState(false);
+    const [modalTransaction, setModalTransaction] = useState(false);
+    const [logTransaction, setLogTransaction] = useState({});
 
     useEffect(() => {
-        if(tabSelected === 'open' || tabSelected === 'history')getFeedbacks();
-        if(tabSelected === 'users')getUsers();
+        if (tabSelected === 'open' || tabSelected === 'history') getFeedbacks();
+        if (tabSelected === 'users') getUsers();
     }, [tabSelected]);
 
     async function getFeedbacks() {
@@ -42,30 +50,101 @@ export function DeveloperActions() {
         setLoading(false);
     }
 
-    async function getUsers(){
+    async function getUsers() {
         setLoading(true);
         const response = await api.get('/web3/developers');
         setUsers(response.data.developers);
         setLoading(false);
     }
 
+    function handleInvite() {
+        if (!wallet.trim()) {
+            toast.error('Digite uma wallet!');
+            return
+        }
+        if (window.ethereum) {
+            inviteUser();
+        } else {
+            toast.error('Você precisa estar em um navegador com provedor Ethereum!')
+        }
+    }
+
+    async function inviteUser() {
+        setModalTransaction(true);
+        setLoadingTransaction(true);
+        Invite(walletConnected, String(wallet).toLowerCase(), 4)
+            .then(async (res) => {
+                setLogTransaction({
+                    type: res.type,
+                    message: res.message,
+                    hash: res.hashTransaction
+                });
+
+                if (res.type === 'success') {
+                    await api.post('/publication/new', {
+                        userId: userData?.id,
+                        type: 'invite-wallet',
+                        origin: 'platform',
+                        additionalData: JSON.stringify({
+                            hash: res.hashTransaction,
+                            walletInvited: wallet,
+                            userType: 4,
+                            userData
+                        }),
+                    });
+                }
+                setLoadingTransaction(false);
+            })
+            .catch(err => {
+                setLoadingTransaction(false);
+                const message = String(err.message);
+                setLogTransaction({
+                    type: 'error',
+                    message: 'Something went wrong with the transaction, please try again!',
+                    hash: ''
+                })
+            })
+
+    }
+
     return (
         <div className="flex flex-col lg:w-[1024px]">
-            <h3 className="font-bold text-white text-lg">Centro de desenvolvimento</h3>
-
             {userData?.userType === 4 && (
-                <div className="flex flex-col p-2 bg-[#0a4303] rounded-md mt-1">
-                    <p className="text-gray-400">Relatório de desenvolvimento</p>
-                    <button
-                        className="font-bold text-white px-3 py-1 rounded-md bg-blue-500 w-fit mt-1"
-                        onClick={() => setModalDevReport(true)}
-                    >
-                        Enviar relatório
-                    </button>
-                </div>
+                <>
+                    <p className="font-bold text-white text-lg">Convidar desenvolvedor</p>
+                    <div className="flex flex-col p-3 rounded-md bg-[#0a4303] mb-5">
+                        <p className="text-white">Para convidar outro desenvolvedor, basta inserir a wallet dele abaixo</p>
+                        <p className="mt-2 font-bold text-blue-500">Wallet</p>
+                        <input
+                            value={wallet}
+                            onChange={(e) => setWallet(e.target.value)}
+                            className="px-3 py-2 rounded-md text-white bg-green-950 max-w-[400px]"
+                            placeholder="Digite aqui"
+                        />
+                        <button
+                            className="font-bold text-white px-3 py-1 rounded-md bg-blue-500 w-fit mt-3"
+                            onClick={handleInvite}
+                        >
+                            Convidar
+                        </button>
+                    </div>
+
+                    <p className="font-bold text-white text-lg">Relatório de desenvolvimento</p>
+                    <div className="flex flex-col p-2 bg-[#0a4303] rounded-md mt-1 mb-5">
+                        <p className="text-gray-400">Envie sua prova de contribuição para o desenvolvimento do sistema</p>
+                        <button
+                            className="font-bold text-white px-3 py-1 rounded-md bg-blue-500 w-fit mt-1"
+                            onClick={() => setModalDevReport(true)}
+                        >
+                            Enviar relatório
+                        </button>
+                    </div>
+                </>
             )}
 
-            <p className="text-gray-400 mt-5">Feedbacks/tasks</p>
+            <h3 className="font-bold text-white text-lg">Centro de desenvolvimento</h3>
+
+            <p className="text-gray-400 mt-1">Feedbacks/tasks</p>
             <div className="flex items-center gap-8 mb-2">
                 <button
                     className={`font-bold py-1 border-b-2 ${tabSelected === 'users' ? ' border-green-600 text-green-600' : 'text-white border-transparent'}`}
@@ -134,6 +213,23 @@ export function DeveloperActions() {
                     userData={userData}
                 />
             )}
+
+            <Dialog.Root open={modalTransaction} onOpenChange={(open) => {
+                if (!loadingTransaction) {
+                    setModalTransaction(open)
+                    setLoading(false);
+                    if (logTransaction.type === 'success') {
+                        toast.success('Apoiador convidado com sucesso!')
+                    }
+                }
+            }}>
+                <LoadingTransaction
+                    loading={loadingTransaction}
+                    logTransaction={logTransaction}
+                />
+            </Dialog.Root>
+
+            <ToastContainer />
         </div>
     )
 }
