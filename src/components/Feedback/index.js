3263b882e8dfe946ae042e97e9da92c5a5162d71
Emanoel Augusto <emanoelaugusto7@gmail.com>
2024-07-18 11:07:14 -0300

    feat: remove animation btn feedback

--- src/components/Feedback/index.js
+++ src/components/Feedback/index.js
@@ -11,7 +11,7 @@ export function Feedback(){
         <div>
             <Dialog.Root open={modal} onOpenChange={(open) => setModal(open)}>
                 <Dialog.Trigger
-                    className="flex items-center gap-5 justify-end px-2 h-10 w-[140px] bg-red-500 absolute bottom-10 left-[-95px] hover:left-0 duration-500 rounded-r-md text-white font-bold"
+                    className="flex items-center gap-5 justify-end px-2 h-10 w-[140px] bg-red-500 absolute bottom-10 left-0 duration-500 rounded-r-md text-white font-bold"
                 >
                     Feedback
                     <MdOutlineFeedback color='white' size={20}/>
