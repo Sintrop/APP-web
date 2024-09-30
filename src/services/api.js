9fdf999a06dbfbc560b991d8594d5d2781a9155a
Emanoel Augusto <emanoelaugusto7@gmail.com>
2024-09-30 13:26:51 -0300

    feat: change port local server

--- src/services/api.js
+++ src/services/api.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export const api = axios.create({
     baseURL: 'https://v5-api-production.up.railway.app'
-    //baseURL: 'http://localhost:3333'
+    //baseURL: 'http://localhost:4000'
 })
