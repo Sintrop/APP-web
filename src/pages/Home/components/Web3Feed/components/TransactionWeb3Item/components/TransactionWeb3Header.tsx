c04bf8ec23945b4bd3562a71df2006eb503c74ab
Emanoel Augusto <emanoelaugusto7@gmail.com>
2025-01-02 17:41:11 -0300

    feat: test jazz icons

--- src/pages/Home/components/Web3Feed/components/TransactionWeb3Item/components/TransactionWeb3Header.tsx
+++ src/pages/Home/components/Web3Feed/components/TransactionWeb3Item/components/TransactionWeb3Header.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Jazzicon } from '@ukstv/jazzicon-react';
 
 interface Props{
     fromWallet: string;
@@ -7,7 +8,9 @@ export function TransactionWeb3Header({fromWallet}: Props){
     return(
         <div className="flex items-center justify-between">
             <div className="flex gap-3">
-                <div className="w-12 h-12 rounded-full bg-container-secondary"/>
+                <div className="w-12 h-12 rounded-full bg-container-secondary">
+                    <Jazzicon address={fromWallet} />
+                </div>
                 <div className="flex flex-col">
                     <p className="text-white text-sm">{fromWallet}</p>
                 </div>
