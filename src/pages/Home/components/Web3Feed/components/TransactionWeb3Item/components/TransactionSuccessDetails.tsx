ff5306b636be685fcde1c6e55e60a2d519246cad
Emanoel Augusto <emanoelaugusto7@gmail.com>
2025-01-03 11:48:09 -0300

    feat: new design decoded input

--- src/pages/Home/components/Web3Feed/components/TransactionWeb3Item/components/TransactionSuccessDetails.tsx
+++ src/pages/Home/components/Web3Feed/components/TransactionWeb3Item/components/TransactionSuccessDetails.tsx
@@ -1,44 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
 import { ParametersTransactionProps } from "../../../../../../../types/transaction";
 
-interface Props{
+interface Props {
     parameters: ParametersTransactionProps[];
     method: string;
 }
-export function TransactionSuccessDetails({parameters, method}: Props){
-    const {t} = useTranslation();
+export function TransactionSuccessDetails({ parameters, method }: Props) {
+    const { t } = useTranslation();
+    const [showDecodedInput, setShowDecodedInput] = useState(false);
 
-    if(!parameters){
-        return <p className="text-white">Nenhum dado adicional para ser exibido</p>
+    function toggleShowDecodedInput() {
+        setShowDecodedInput(old => !old);
     }
 
-    if(parameters.length === 0){
-        return(
-            <p className="text-white">Nenhum dado adicional para ser exibido</p>
-        )
-    }
-    
     return (
         <div className="flex flex-col gap-1 mt-2">
-            {parameters.map(item => (
-                <DecodedInputItem
-                    key={item.name}
-                    input={item}
-                />
-            ))}
+            <button
+                onClick={toggleShowDecodedInput}
+                className="text-white text-sm underline text-start w-fit"
+            >
+                {showDecodedInput ? 'Ocultar dados brutos' : 'Exibir dados brutos'}
+            </button>
+            <div className={`flex flex-col gap-1 ${showDecodedInput ? 'h-[200px]' : 'h-0'} duration-300`}>
+                {showDecodedInput && (
+                    <DecodedInput
+                        parameters={parameters}
+                    />
+                )}
+            </div>
         </div>
     )
 }
 
-interface DecodedInputProps{
+interface DecodedInputProps {
+    parameters: ParametersTransactionProps[];
+}
+function DecodedInput({ parameters }: DecodedInputProps) {
+
+    if (!parameters) {
+        return <p className="text-white">Nenhum dado adicional para ser exibido</p>
+    }
+
+    if (parameters.length === 0) {
+        return (
+            <p className="text-white">Nenhum dado adicional para ser exibido</p>
+        )
+    }
+
+    return (
+        <div className="flex flex-col mt-1">
+            <div className="flex h-10 bg-container-secondary rounded-t-md px-3 items-center py-1">
+                <div className="flex w-[20%] border-r border-container-primary h-full items-center">
+                    <p className="text-white font-bold text-sm">Name</p>
+                </div>
+                <div className="flex w-[80%] pl-3 h-full items-center">
+                    <p className="text-white font-bold text-sm">Data</p>
+                </div>
+            </div>
+            <div className="flex flex-col h-[155px] overflow-y-auto">
+                {parameters.map(item => (
+                    <DecodedInputItem
+                        key={item.name}
+                        input={item}
+                    />
+                ))}
+            </div>
+        </div>
+    )
+}
+
+interface DecodedInputItemProps {
     input: ParametersTransactionProps;
 }
-function DecodedInputItem({input}: DecodedInputProps){
+function DecodedInputItem({ input }: DecodedInputItemProps) {
     return (
-        <div className="w-full px-3 h-10 bg-container-secondary rounded-md flex items-center gap-3">
-            <p className="text-white text-sm">{input.name}</p>
-            <p className="text-white text-sm truncate">{input.value}</p>
+        <div className="flex h-10 bg-container-secondary px-3 items-center py-1 border-b border-container-primary">
+            <div className="flex w-[20%] h-full items-center">
+                <p className="text-white truncate text-sm">{input.name}</p>
+            </div>
+            <div className="flex w-[80%] pl-3 h-full items-center">
+                <p className="text-white truncate text-sm">{input.value}</p>
+            </div>
         </div>
     )
 }
